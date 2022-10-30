7ad7c66494cb98438fcb89040e024c6a24035d53
Arkhin <nabillouh62110@gmail.com>
2022-10-30 18:10:01 +0100

    gestion des commande admin et user

--- resources/views/Apropos.blade.php
+++ resources/views/Apropos.blade.php
@@ -4,6 +4,49 @@
 
 @section('content')
 
-<div>A propos</div>
+<div class="flex justify-evenly my-16">
+
+
+    <div class="bg-white w-2/5 border-4 border-orange-500 rounded-lg">
+       <h1> Nous somme les Power Ranger, justicier de l'an 2000</h1>
+
+
+        <p>
+            Avec nous et nos mecha dinosaure du future, nous voyagons
+            d'epoque en epoque pour anéantire le vil Zurglock
+        </p>
+
+        <p>
+            Mais pour votre grand bonheur, nous avons decidé de faire de 
+            l'impression 3D
+        </p>
+
+        <p>Alors hesitez pas a vider votre compte en banque chez nous</p>
+        <p>L'essence pour les mecha coute un bras et avec la pénurie 
+            et tout ca....
+        </p>
+
+        <p>
+            m'enfin, ta compris BG x)
+        </p>
+    </div>
+
+
+    <div class="bg-white w-2/5 border-4 border-orange-500 rounded-lg">
+        <p>Pour toutes Question hors devis</p>
+        <p>Merci de nous contacter au :</p>
+        <div>
+            <h5>Par téléphone au :</h5>
+            
+            <p> 07.57.83.48.69</p>
+        </div>
+
+        <div>
+            <h5>Par mail a :</h5>
+            <a href="mailto: vkg.maker3d@gmail.com">vkg.maker3d@gmail.com,</a>
+        </div>
+        
+    </div>
+</div>
 
 @endsection
